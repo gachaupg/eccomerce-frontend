9060db7e3dfa992135d32e61950de4313b925b28
Ebenezer Okello <okelloebenezer403@gmail.com>
2023-07-23 18:27:28 +0300

    first commit

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../../slices/authSlice";
+import { Link, useNavigate } from "react-router-dom";
+import { StyledForm } from "./StyledForm";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(user);
+    dispatch(loginUser(user));
+    navigate("/");
+
+  };
+
+  return (
+    <>
+      <StyledForm onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <button>
+          {auth.loginStatus === "pending" ? "Submitting..." : "Login"}
+        </button>
+        {auth.loginStatus === "rejected" ? <p>{auth.loginError}</p> : null}
+      <p>if no account <Link to='/register'>
+        Register here
+        </Link></p>
+      </StyledForm>
+    </>
+  );
+};
+
+export default Login;
